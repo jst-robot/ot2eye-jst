2efeb77ca79da8f19ff20f3369d66f257afaaa55
Banana-Tachi <tachikoki@gmail.com>
2022-11-16 19:55:48 +0900

    縮小前画像を与えたらトリミング画像生成まで

--- README.md
+++ README.md
@@ -32,3 +32,66 @@ python3 -m pip install -r requirements.txt
 - yaml
 
 - argparse
+
+
+
+## 変更点
+
+yolov5/utils/plots.py
+
+参考：https://qiita.com/fujioka244kogacity/items/e5acf8d9bb728e7d1bcc
+
+* 99行目をコメントアウトし，100行目追加
+
+  ```
+  cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
+  ```
+
+  ↓
+
+  ```
+  cv2.rectangle(self.im, p1, p2, color, thickness=1, lineType=cv2.LINE_AA)
+  ```
+
+* 106行目コメントアウト
+
+  ```
+  cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+  ```
+
+  ↓
+
+  ```
+  # cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+  ```
+
+* 107~113行目をコメントアウトし，114~119行目追加
+
+  ```
+  cv2.putText(self.im,
+              label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+              0,
+              self.lw / 3,
+              txt_color,
+              thickness=tf,
+              lineType=cv2.LINE_AA)
+  ```
+
+  ↓
+
+  ```
+  cv2.putText(self.im,
+              label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+              cv2.FONT_HERSHEY_SIMPLEX,
+              self.lw / 6,
+              txt_color,
+              thickness=1)
+  ```
+
+  
+
+```
+cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
+```
+
+↓
--- ot2eye.py
+++ ot2eye.py
@@ -1,29 +1,130 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import subprocess
 import argparse
-import torch
+import cv2
+from scripts.trim_tip_rack import Trim_Tip_Rack
+# import torch
 
 
 class OT2Eye():
-	def __init__(self):
-		model = torch.hub.load("ultralytics/yolov5", "yolov5s")
-		# img = "model/detect_tip_20220624/weights/best.pt"
-		# img = "./yolov5/data/images/bus.jpg"
-		img = "./dataset/20220718_small/IMG_0278.jpeg"
+	def __init__(self, img_dir, out_dir, model_labware, model_tip, threshold, train_yaml):
+		# img_dir: 推論画像ディレクトリ
+		# out_dir: 結果保存ディレクトリ
+		# threshold: 検出閾値
+		WIDTH_SMALL = 640 # 縮小後画像幅
+		HIGHT_SMALL = 480 # 縮小後画像高
+		TIP_RACK_LABEL_NAME = "tip_rack"
+		self.OUT_DIR_IMG_RESIZE = "images_resize" #縮小画像保存ディレクトリ
+		self.OUT_DIR_DETECT_LABWARE = "detect_labware" #ラボウェア検出結果ディレクトリ
+		self.OUT_DIR_IMG_TRIM = "image_trim_tip_rack"# チップラックトリミング画像ディレクトリ
 
-		results = model(img)
+		# 画像ディレクトリ存在確認
+		if not os.path.isdir(img_dir):
+			print("No such directory \"{}\"".format(img_dir))
+			return
 
-		results.show()
+		# 保存先ディレクトリ処理
+		if not os.path.isdir(out_dir): #存在しなければ生成
+			os.mkdir(out_dir)
+		else: #存在すれば連番で代わりを生成
+			# ディレクトリパスの最後に"/"があれば消す
+			while out_dir[-1]=="/":
+				out_dir = out_dir[:-1]
+			# 連番ディレクトリ名存在確認
+			for i in range(1,256):
+				out_dir_cdd = out_dir+str(i)
+				if not os.path.isdir(out_dir_cdd):
+					os.mkdir(out_dir_cdd)
+					print("The directory \"{}\" already exists. \"{} was made instead. \"".format(out_dir, out_dir_cdd))
+					out_dir = out_dir_cdd
+					break
+		
+		# 画像の縮小＆保存
+		self.resize_save_img(img_dir, out_dir, WIDTH_SMALL)
+
+		print("\n\nfin\n\n")
+		# ラボウェア検出
+		subprocess.run(["python3", "yolov5/detect.py",\
+				# 検出対象画像ディレクトリ
+				"--source", out_dir+"/"+self.OUT_DIR_IMG_RESIZE,\
+				# 検出結果出力先ディレクトリ
+				"--project", out_dir,\
+				# 検出結果ディレクトリ名
+				"--name", self.OUT_DIR_DETECT_LABWARE,\
+				# 検出モデル
+				"--weights", model_labware,\
+				# 検出閾値
+				"--conf", str(threshold),\
+				# 推論結果ラベル出力
+				"--save-txt",\
+				# 推論結果確率出力
+				"--save-conf",\
+				# 検出結果上書き
+				"--exist-ok"])
+
+		print("\n\nfin\n\n")
+
+		# 検出結果からトリミング画像生成
+		if not os.path.isdir(out_dir+"/"+self.OUT_DIR_IMG_TRIM): #存在しなければ生成
+			os.mkdir(out_dir+"/"+self.OUT_DIR_IMG_TRIM)
+		trim = Trim_Tip_Rack(\
+				# トリミング前画像ディレクトリ
+				img_dir,\
+				# ラボウェア検出結果ラベルディレクトリ
+				out_dir+"/"+self.OUT_DIR_DETECT_LABWARE+"/labels/",\
+				# 学習時yamlファイル
+				train_yaml,\
+				# トリミング結果画像ディレクトリ
+				out_dir+"/"+self.OUT_DIR_IMG_TRIM+"/",\
+				# チップラックのラベル名
+				TIP_RACK_LABEL_NAME,\
+				# トリミング結果画像幅
+				WIDTH_SMALL,\
+				# トリミング結果画像高
+				HIGHT_SMALL)
+
+
+
+	# 画像の縮小＆保存
+	def resize_save_img(self, img_dir, out_dir, width):
+		imgs = os.listdir(path=img_dir)
+
+		for num in range(len(imgs)):
+			# read image
+			img = cv2.imread(img_dir+"/"+imgs[num])
+			# resize
+			h, w = img.shape[:2]
+			height = round(h * (width / w))
+			img = cv2.resize(img, dsize=(width, height))
+			# save
+			dir_name = out_dir+"/"+self.OUT_DIR_IMG_RESIZE
+			if not os.path.isdir(dir_name):
+				os.mkdir(dir_name)
+			cv2.imwrite(dir_name+"/"+imgs[num], img)
+
+
+
+		# model = torch.hub.load("ultralytics/yolov5", "yolov5s")
+		# # img = "model/detect_tip_20220624/weights/best.pt"
+		# # img = "./yolov5/data/images/bus.jpg"
+		# img = "./dataset/20220718_small/IMG_0278.jpeg"
+
+		# results = model(img)
+
+		# results.show()
 
 if __name__ == '__main__':
 	# argument
-	# prs = argparse.ArgumentParser()
+	prs = argparse.ArgumentParser()
 	# prs.add_argument("--model-dir", type=str, required=True,
 	# 		help="directory path of detection model file.")
-	# prs.add_argument("--image-dir", type=str, required=True,
-	# 		help="directory path of image files.")
-	# prs.add_argument("--out-dir", type=str, required=True,
-	# 		help="directory path of output files.")
-	# args = prs.parse_args()
+	prs.add_argument("--image-dir", type=str, required=True,
+			help="directory path of image files.")
+	prs.add_argument("--out-dir", type=str, required=True,
+			help="directory path of output files.")
+	args = prs.parse_args()
 
-	ot2eye = OT2Eye()
+	# ot2eye = OT2Eye(args.image_dir, args.out_dir, "./model/detect_labware_20220624/weights/best.pt", "./model/detect_tip_20220624/weights/best.pt", 0.7, "./dataset_20220624_small_notip.yaml")
+	ot2eye = OT2Eye(args.image_dir, args.out_dir, "../obj_rec/model/exp_20220624_small_notip/weights/best.pt", "./model/detect_tip_20220624/weights/best.pt", 0.7, "./dataset_20220624_small_notip.yaml")
